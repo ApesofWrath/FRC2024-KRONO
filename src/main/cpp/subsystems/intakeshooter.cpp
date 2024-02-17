91aef1b54ea2594ee2ca9c1519a79b76d0290c0d
Parkman145 <ParkerLearmonth@gmail.com>
2024-02-16 21:46:53 -0800

    i like turneries - s

--- src/main/cpp/subsystems/intakeshooter.cpp
+++ src/main/cpp/subsystems/intakeshooter.cpp
@@ -10,7 +10,7 @@ m_intakeMotorRight(kIntakeMotorRight),
 m_shooterMotorLeft(kMotorShooterLeft, rev::CANSparkMax::MotorType::kBrushless),
 m_shooterMotorRight(kMotorShooterRight, rev::CANSparkMax::MotorType::kBrushless),
 m_rotationMotor(kIntakeRotationMotor, rev::CANSparkMax::MotorType::kBrushless),
-m_BeambreakCanifierIntake(kBeambreakCanifier)
+m_BeambreakCanifier(kBeambreakCanifier)
 {
 
     //Krakens
@@ -92,16 +92,12 @@ void intakeshooter::Periodic() {
             m_intakeMotorLeft.SetControl(m_velocityIntake.WithVelocity(2.5_tps)); // set the speed of the intake motor
             m_shooterMotorLeftController.SetReference(0.25, rev::CANSparkMax::ControlType::kDutyCycle); // set the speed of the shooter motor (worse api b/c REV is cringe)
             m_rotationMotorController.SetReference(0, rev::CANSparkMax::ControlType::kPosition); // set the angle 
-            if (m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMF)) {
-                currentIntakeshooterState = intakeshooterStates::HOLDING;
-            }
+            currentIntakeshooterState = m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMF) ? intakeshooterStates::HOLDING : intakeshooterStates::IDLE; // if the canifier's limit forward input is tripped, switch to holding
             break;
         case intakeshooterStates::INTAKING:
             m_intakeMotorLeft.SetControl(m_velocityIntake.WithVelocity(7.5_tps)); // set the speed of the intake motor (TODO: tune speed)
             m_rotationMotorController.SetReference(30, rev::CANSparkMax::ControlType::kPosition); // set the angle (TODO: tune angle)
-            if (m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMF)) {
-                currentIntakeshooterState = intakeshooterStates::HOLDING;
-            }
+            currentIntakeshooterState = m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMF) ? intakeshooterStates::HOLDING : intakeshooterStates::INTAKING; // if the canifier's limit forward input is tripped, switch to holding
             break;
         case intakeshooterStates::HOLDING:
             m_intakeMotorLeft.SetControl(m_velocityIntake.WithVelocity(0.0_tps)); // set the speed of the intake motor
@@ -114,9 +110,7 @@ void intakeshooter::Periodic() {
         case intakeshooterStates::FIRE: //in the hole
             m_intakeMotorLeft.SetControl(m_velocityIntake.WithVelocity(1.0_tps)); // set the speed of the intake motor (TODO: tune speed)
             m_rotationMotorController.SetReference(30, rev::CANSparkMax::ControlType::kPosition); // set the angle (TODO: tune angle)
-            if (m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMR)) {
-                currentIntakeshooterState = intakeshooterStates::IDLE;
-            }
+            currentIntakeshooterState = m_BeambreakCanifier.GetGeneralInput(ctre::phoenix::CANifier::LIMR) ? intakeshooterStates::IDLE : intakeshooterStates::FIRE; // if the canifier's limit backward input is tripped, switch to idle
             break;
     }
 }
