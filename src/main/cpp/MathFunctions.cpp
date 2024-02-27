0d63727587d47ae4be82db2742686fc6c827728b
Parkman145 <ParkerLearmonth@gmail.com>
2024-02-26 21:36:38 -0800

    Remove old commented code

--- src/main/cpp/MathFunctions.cpp
+++ src/main/cpp/MathFunctions.cpp
@@ -18,11 +18,3 @@ double MathFunctions::lerpPoints( std::vector<double> &xData, std::vector<double
    int i = 0; while ( x > xData[i] ) i++; // create a variable i and increment it until it is the index of the largest x cord under x
    return std::lerp(yData[i-1], yData[i], (x-xData[i])/(x-xData[i+1])); // use lerp w/ t = (distance from x to xLeft)/(distance from x to xRight)
 }
-
-// double MathFunctions::getPointed(std::vector<std::vector<double>>){
-//     minVal = 
-// }
-
-// double MathFunctions::POVDistance(std::vector<double> origin, std::vector<double> angle, std::vector<double> target){
-//     centered = 
-// }
