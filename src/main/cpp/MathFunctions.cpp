38409511e5c3cbcf73c3d9e30cbd6523490dfae8
Parkman145 <ParkerLearmonth@gmail.com>
2024-02-26 21:32:09 -0800

    write a lerp pointer that works with points

--- src/main/cpp/MathFunctions.cpp
+++ src/main/cpp/MathFunctions.cpp
@@ -14,6 +14,11 @@ double MathFunctions::negativeSqrt(double x) {
     return (sqrt(abs_x))*sign;
 }
 
+double MathFunctions::lerpPoints( std::vector<double> &xData, std::vector<double> &yData, double x ) { // Assumes that xData has at least two elements, is sorted, and is increasing
+   int i = 0; while ( x > xData[i] ) i++; // create a variable i and increment it until it is the index of the largest x cord under x
+   return std::lerp(yData[i-1], yData[i], (x-xData[i])/(x-xData[i+1])); // use lerp w/ t = (distance from x to xLeft)/(distance from x to xRight)
+}
+
 // double MathFunctions::getPointed(std::vector<std::vector<double>>){
 //     minVal = 
 // }
