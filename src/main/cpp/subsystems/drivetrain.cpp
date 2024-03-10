e2d7775ccfc411f1bfa64f638dcf8a7bd8c05288
Parkman145 <ParkerLearmonth@gmail.com>
2024-03-10 16:48:02 -0700

    Tune drive PID + add info for logging

--- src/main/cpp/subsystems/drivetrain.cpp
+++ src/main/cpp/subsystems/drivetrain.cpp
@@ -73,7 +73,8 @@ void drivetrain::SwerveDrive(units::meters_per_second_t xSpeed,
 
     //frc::SmartDashboard::PutNumber("xSpeed", xSpeed.value());
     // frc::SmartDashboard::PutNumber("ySpeed", ySpeed.value());
-    // frc::SmartDashboard::PutNumber("zRotation", zRot.value());
+    frc::SmartDashboard::PutNumber("zRotation", zRot.value());
+    frc::SmartDashboard::PutNumber("zRotation actual", m_navX.GetRate() * -1.0);
     frc::SmartDashboard::PutNumber("Robot Rotation", m_navX.GetRotation2d().Degrees().value());
     frc::SmartDashboard::PutNumber("navX Yaw", m_navX.GetYaw());
     frc::SmartDashboard::PutNumber("Od Rob Rot", m_odometry.GetEstimatedPosition().Rotation().Degrees().value());
--- src/main/cpp/swerveModule.cpp
+++ src/main/cpp/swerveModule.cpp
@@ -44,9 +44,9 @@ swerveModule::swerveModule(const double module[])
     m_driveController.SetFeedbackDevice(m_encoderDrive);
     m_turnController.SetFeedbackDevice(m_encoderTurnIntegrated);
 
-    m_driveController.SetP(0.1);
+    m_driveController.SetP(0.2);
     m_driveController.SetI(0);
-    m_driveController.SetD(0);
+    m_driveController.SetD(0.007);
     //m_driveController.SetFF(1/107.9101*2); //(0.5*1023.0)/(22100.0*0.5)
     m_driveController.SetFF(1.0/4.6);
     m_driveController.SetOutputRange(-1.0F, 1.0F);
@@ -93,7 +93,7 @@ void swerveModule::SetDesiredState(const frc::SwerveModuleState& referenceState)
     m_encoderTurnIntegrated.SetPosition(m_encoderTurn.GetAbsolutePosition().GetValueAsDouble() * kRotationsToDegrees);
     m_turnController.SetReference(turnOutput, rev::CANSparkMax::ControlType::kPosition);
 
-    frc::SmartDashboard::PutNumber("Target Wheel Speed", targetWheelSpeed.value());
+    frc::SmartDashboard::PutNumber("Target Wheel Speed" + std::to_string(m_motorTurn.GetDeviceId()), targetWheelSpeed.value());
     frc::SmartDashboard::PutNumber("Target Motor Speed", targetMotorSpeed.value());
 }
 
