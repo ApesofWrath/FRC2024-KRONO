b74166e28a6ff8e20298d42ce48c8e4049b84905
ShaharGur1 <shahar.seal@gmail.com>
2024-03-16 13:09:34 -0700

    Fix wrong speed conversion

--- src/main/cpp/subsystems/drivetrain.cpp
+++ src/main/cpp/subsystems/drivetrain.cpp
@@ -13,7 +13,7 @@ drivetrain::drivetrain() {
         [this](){ return GetOdometry(); }, // Robot pose supplier
         [this](frc::Pose2d pose){ ResetOdometry(pose); }, // Method to reset odometry (will be called if your auto has a starting pose)
         [this](){ return GetRobotRelativeSpeeds(); }, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        [this](frc::ChassisSpeeds speeds){ SwerveDrive(speeds.vx, speeds.vy, speeds.omega, false); }, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        [this](frc::ChassisSpeeds speeds){ DriveRobotRelativeSpeeds(speeds); }, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
         HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
             PIDConstants(0.5, 0.0, 0.0), // Translation PID constants
             PIDConstants(10.0, 0.0, 0.0), // Rotation PID constants
@@ -36,24 +36,6 @@ drivetrain::drivetrain() {
     );
 }
 
-// Resets the gyro when function run
-void drivetrain::resetGyro() {
-    m_navX.ZeroYaw();
-}
-
-// Slow constant value
-void drivetrain::slowDown() {
-    kslowConst = 0.5;
-    printf("Slow Func");
-}
-
-// Normal speed value (should always be 1.0)
-void drivetrain::normalSpeed() {
-    kslowConst = 1.0;
-    printf("Normal Func");
-}
-
-// Sets Desired States of the swerve modules for swervedrive
 void drivetrain::SwerveDrive(units::meters_per_second_t xSpeed,
                              units::meters_per_second_t ySpeed,
                              units::radians_per_second_t zRot,
@@ -94,6 +76,11 @@ void drivetrain::UpdateOdometry() {
                       m_rearLeft.GetPosition()});
 }
 
+// Resets the gyro when function run
+void drivetrain::resetGyro() {
+    m_navX.ZeroYaw();
+}
+
 // Perodically (Constantly runs during periodic), updates the odometry of the swervedrive
 frc::Pose2d drivetrain::GetOdometry() {
     return m_odometry.GetEstimatedPosition();
@@ -114,8 +101,31 @@ void drivetrain::ResetOdometry(frc::Pose2d initPose) {
 }
 
 frc::ChassisSpeeds drivetrain::GetRobotRelativeSpeeds(){
-    frc::ChassisSpeeds speeds = m_kinematics.ToChassisSpeeds(m_frontRight.GetState(), m_rearRight.GetState(), m_frontLeft.GetState(), m_rearLeft.GetState());
-    return frc::ChassisSpeeds::FromFieldRelativeSpeeds(speeds.vx, speeds.vy, speeds.omega, m_navX.GetRotation2d());
+    return m_kinematics.ToChassisSpeeds(m_frontRight.GetState(), m_rearRight.GetState(), m_frontLeft.GetState(), m_rearLeft.GetState());
+}
+
+void drivetrain::DriveRobotRelativeSpeeds(frc::ChassisSpeeds robotRelativeSpeeds) {
+    frc::ChassisSpeeds targetSpeeds = frc::ChassisSpeeds::Discretize(robotRelativeSpeeds, 0.2_s);
+    auto moduleStates = m_kinematics.ToSwerveModuleStates(targetSpeeds);
+    auto [frontRight, rearRight, frontLeft, rearLeft] = moduleStates;
+
+    m_frontRight.SetDesiredState(frontRight);
+    m_rearRight.SetDesiredState(rearRight);
+    m_frontLeft.SetDesiredState(frontLeft);
+    m_rearLeft.SetDesiredState(rearLeft);
+}
+
+
+// Slow constant value
+void drivetrain::slowDown() {
+    kslowConst = 0.5;
+    printf("Slow Func");
+}
+
+// Normal speed value (should always be 1.0)
+void drivetrain::normalSpeed() {
+    kslowConst = 1.0;
+    printf("Normal Func");
 }
 
 void drivetrain::Periodic() {
